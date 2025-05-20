b40d55c400e2f866d1f51f410d4c0156dde5011b
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-05-20 17:20:11 +0530

    Basic Java Syntax

---
+++ JavaOperators.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class JavaOperators {
+    public static void main (String args[]) {
+        int A = 5;
+        int B = 10;
+        System.out.println("The addition of A and B is " + (A + B));
+        System.out.println("The multiplicaton  of A and B is " + (A * B));
+        System.out.println("The division of A and B is " + (A/B));
+        System.out.println("The subtraction of A and B is " + (A - B));
+        System.out.println("The modulo of A and B is " + (A%B));
+
+
+
+
+    }
+}
