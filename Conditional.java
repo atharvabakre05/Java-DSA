5a5df872779722522d7d07b9ff8ca7b6533303b1
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-05-23 23:48:08 +0530

    Built a Tax Calculator

--- Conditional.java
+++ Conditional.java
@@ -17,15 +17,32 @@ public class Conditional {
     //     else{
     //         System.out.println("B is the largest");
     //         }
+        
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+        // if(number%2==0) {
+        //     System.out.println("The entered number is even");
+        // }
+        // else{
+        //     System.out .println("The entered number is odd");
+        // }
 
-        int a = 9;
-        if(a%2==0) {
-            System.out.println("A is an even number");
-        }
-        else{
-            System.out .println("A is an odd number");
-        }
-    }
+
+    //     Scanner sc = new Scanner(System.in);
+    //     int income = sc.nextInt();
+    //     int tax;
+    //     if(income<500000){
+    //         tax = 0;
+    //     }
+    //     else if(income>=500000 && income<1000000) {
+    //         tax = (int)(income*0.2);
+    //     }
+    //     else{
+    //         tax = (int)(income*0.3);
+    //     }
+    //     System.out.println("The tax you are paying is: "+ tax);
+    // }
+
 }
 
       
