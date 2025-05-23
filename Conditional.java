bd659a4d7ba66825a094f9ba6e5b633b1620d721
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-05-23 23:08:29 +0530

    Learnt about Conditional statements in java

---
+++ Conditional.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Conditional {
+    public static void main(String args[]) {
+    //     int age = 20;
+    //     if(age>= 18) {
+    //         System.out.println("Eligible for voting");
+    //     }
+    //     else {
+    //         System.out.println("Not eligible");
+    // //     }
+
+    //   int A = 75;
+    //     int B = 80;
+    //     if(A>B) {
+    //         System.out.println("A is the largest");
+    //     }
+    //     else{
+    //         System.out.println("B is the largest");
+    //         }
+
+        int a = 9;
+        if(a%2==0) {
+            System.out.println("A is an even number");
+        }
+        else{
+            System.out .println("A is an odd number");
+        }
+    }
+}
+
+      
+    
