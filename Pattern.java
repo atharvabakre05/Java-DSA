878b5dd7c20b7480d630e820ba4116b99013a557
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-27 22:54:57 +0530

    Character Pattern

--- Pattern.java
+++ Pattern.java
@@ -11,12 +11,52 @@ public class Pattern {
         // }
 
         // REVERTED STAR PATTERN
+//         int n = 4;
+//         for(int line = 1; line<=4; line++) {
+//             for(int star=1; star<=(n-line+1); star++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+         //HALF PYRAMID PATTERN
+        //  int line;
+        //  int num;
+        //  for(line=1;line<=4;line++) {
+        //     for(num=1;num<=line;num++) {
+        //         System.out.print(num);
+        //     }
+        //     System.out.println();
+        //  }
+
+        //CHARACTER PATTERN
+
         int n = 4;
-        for(int line = 1; line<=4; line++) {
-            for(int star=1; star<=(n-line+1); star++) {
-                System.out.print("*");
+        char ch = 'A';
+
+        for(int line = 1; line<=n; line++) {
+            
+            for(int chars=1; chars<=line; chars++){
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
