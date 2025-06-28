a1f20d3a121f181c589ec669fd8563635ed5efc1
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-28 22:04:14 +0530

    Swapped 2 numbers

--- Functions/Functions.java
+++ Functions/Functions.java
@@ -11,16 +11,39 @@ public class Functions {
         int sum = num1 + num2;
         return sum; 
     }
+    
+    public static void swap(int a, int b) {
+
+        int temp = a;
+        a = b;
+        b = temp; 
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);   
+    }
+
+    public static int product(int a, int b){
+        int mul;
+        mul = a*b;
+        System.out.println("The product is : " +mul);
+        return mul;
+    }
+
 
     public static void main(String[] args) {
-        printHelloWorld();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter second number:");
-        int b = sc.nextInt();
-        int sum = calculateSum(a,b);
-        System.out.println("The sum is: " + sum);
+        // printHelloWorld();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter first number:");
+        // int a = sc.nextInt();
+        // System.out.println("Enter second number:");
+        // int b = sc.nextInt();
+        // int sum = calculateSum(a,b);
+        // System.out.println("The sum is: " + sum);
+        // swap(5, 10);
+        
+        product(10,10);
+
+
 
     }
 }
