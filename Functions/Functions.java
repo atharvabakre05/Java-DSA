e925240d687431c139cc71193bb7fcf0003a40d4
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-28 18:09:47 +0530

    Started with Functions

---
+++ Functions/Functions.java
@@ -0,0 +1,9 @@
+public class Functions {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String args []) {
+        printHelloWorld();
+
+    }
+}
