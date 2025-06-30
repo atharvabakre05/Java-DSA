346ef623e4d3ae5af2508a0004d81f013f0a9b59
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-06-30 19:19:02 +0530

    Factorial program

--- Functions/Functions.java
+++ Functions/Functions.java
@@ -29,9 +29,18 @@ public class Functions {
         return mul;
     }
 
-
+    public static int factorial(int n) {
+        int fact = 1;
+        for(int i = 1; i<=n; i++) {
+            fact = fact*i;
+        }
+        return fact;
+    }
     public static void main(String[] args) {
+        //PRINT HELLO WORLD FUNCTION
         // printHelloWorld();
+
+        //SUM OF TWO NUMBER FUNCTION
         // Scanner sc = new Scanner(System.in);
         // System.out.println("Enter first number:");
         // int a = sc.nextInt();
@@ -39,11 +48,25 @@ public class Functions {
         // int b = sc.nextInt();
         // int sum = calculateSum(a,b);
         // System.out.println("The sum is: " + sum);
+
+        //SWAP FUNCTION
         // swap(5, 10);
+
+        //PRODUCT FUNCTION
+        // product(10,10);
+
+        //FACTORIAL FUNCTION
         
-        product(10,10);
+        System.out.println(factorial(4));
+        
 
 
 
-    }
+
+
+
+
+
+
+        }
 }
