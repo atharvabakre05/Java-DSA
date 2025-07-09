f59bedcfb902bbe69046e6160f7cb27eaa011f76
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-07-09 15:54:23 +0530

    Practicing method questions

---
+++ Functions/Practice.java
@@ -0,0 +1,10 @@
+public class Practice{
+
+    public static void average(int a, int b, int c) {
+        int avg = (a+b+c)/3;
+        System.out.println(avg);
+    }
+    public static void main(String [] args) {
+        average(10, 10, 10);
+    }
+}
