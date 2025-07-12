12c1111c871a60a2acf3bd01b79a80e0467d9c5d
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-07-12 23:18:17 +0530

    Inverted Half-Pyramid with numbers

--- Pattern Adv/pattern.java
+++ Pattern Adv/pattern.java
@@ -35,9 +35,20 @@ public class pattern{
     }
 
 
+//INVERTED HALF-PYRAMID with Numbers
+    public static void inverted_half_pyramid_numbers(int n) {
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String [] args) {
         // hollow_rectangle(4, 5);
-        inverted_half_pyramid(5);
+        // inverted_half_pyramid(5);
+        inverted_half_pyramid_numbers(5);
 
 
 
