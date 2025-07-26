49a6d2af30994c7dd35366e9aebc1c56a1746e02
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-07-26 21:38:04 +0530

    SubArrays

--- Arrays/Arrays2.java
+++ Arrays/Arrays2.java
@@ -23,35 +23,40 @@ public class Arrays2 {
     }
 
 
+    public static void prifixSubarrays(int num[]) {
+        int prefix[] = new int[num.length];
+        prefix[0] = num[0];
+
+        for(int i=1; i<num.length; i++){
+            prefix[i] = prefix[i-1] + num[i];
+        }
+
+        System.out.print("Prefix sum Array: ");
+        for(int i=0; i<prefix.length; i++) {
+            System.out.print(prefix[i] + " ");
+        }
+        System.out.println();
+    }
 
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+
+
+
         //SUM OF SYBARRAY [BRUTE FORCE]
-        int num[] = {2, -4, 6, 8, -10, 7};
-        printSumofSubArrays(num);
+        // int num[] = {2, -4, 6, 8, -10, 7};
+        // printSumofSubArrays(num);
 
 
+        //BREAK //25-07-25
 
 
+        //SUM OF ARRAY USING PREFIX ARRAY {EFFICIENT}
+        int num[] = {2, 5, 7, 10, 9};
+        prifixSubarrays(num);
 
 
-
-
-//BREAK
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
     }
 }
