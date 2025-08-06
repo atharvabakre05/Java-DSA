cc962b5294cca2cdc9631817148d6920e4cf30f4
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-08-07 00:09:06 +0530

    Checked isALreadySorted or not

--- Sorting/Sorts.java
+++ Sorting/Sorts.java
@@ -2,17 +2,30 @@ import java.util.*;
     public class Sorts{
         public static void BubbleSort(int arr[]) {
             int n = arr.length;
+            boolean isAlreadysortArray = true; //Assuming the array is sorted
 
-            for(int turn=0; turn<=n-2; turn++){
+            for(int turn=0; turn<=n-2; turn++){             //outer loop
+                boolean swapped = false;
+
                 for(int j=0; j<=n-2-turn;j++) {
-
                     if(arr[j]>arr[j+1]){
                         int temp = arr[j];
                         arr[j] = arr[j+1];
                         arr[j+1] = temp;
-                    }                    
+                        swapped = true;
+                        isAlreadysortArray = false;                    
+
+                    }
+                    if(!swapped){
+                        break;
+                    }
+                    }
+                if(isAlreadysortArray) {
+                    System.out.print("The array is already sorted");
                 }
+                    
             }
+
         }
 
         public static void printArr(int arr[]) {
