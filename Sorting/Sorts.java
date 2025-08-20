baf81d51be7474434bf76d362531e329d6312da1
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-08-20 11:46:15 +0530

    Selection sorrt implemented

--- Sorting/Sorts.java
+++ Sorting/Sorts.java
@@ -1,40 +1,51 @@
-import java.util.*;
-public class Sorts{
-    public static void bubbleSort(int arr[]){
-        for(int turn=0; turn<arr.length-1; turn++){
-            boolean isSwapped = false;
+    import java.util.*;
+    public class Sorts{
+        // public static void bubbleSort(int arr[]){
+        //     for(int turn=0; turn<arr.length-1; turn++){
+        //         boolean isSwapped = false;
 
-            for(int j=0; j<arr.length-1-turn; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    isSwapped = true;
-                }
-            }
-            if(!isSwapped){
-                if(turn==0){
-                System.out.println("The array is already sorted");
-                break;
-                }
-               
-            }
+        //         for(int j=0; j<arr.length-1-turn; j++){
+        //             if(arr[j]>arr[j+1]){
+        //                 int temp = arr[j];
+        //                 arr[j] = arr[j+1];
+        //                 arr[j+1] = temp;
+        //                 isSwapped = true;
+        //             }
+        //         }
+        //         if(!isSwapped){
+        //             if(turn==0){
+        //             System.out.println("The array is already sorted");
+        //             break;
+        //             }
+                
+        //         }
 
+        //     }
+
+        public static void selectionSort(int arr[]){
+            for(int i=0; i<arr.length-1; i++) {
+                int minPos = i;
+                for(int j=i+1; j<arr.length-1; j++) {
+                    if(arr[j]<arr[minPos]){
+                        minPos = j;
+                    }
+                }
+                //swap
+                int temp = arr[minPos];
+                arr[minPos] = arr[i];
+                arr[i] = temp;
+            }
         }
 
-    }
+        public static void displayArray(int arr[]){
+            for(int i=0; i<arr.length; i++){
+                System.out.print(arr[i] + " ");
+            }
+        }
 
-    public static void displayArray(int arr[]) {
-        System.out.println("The Sorted array is");
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+        public static void main(String args[]) {
+            int arr[] = {1,99,2,33,100};
+            selectionSort(arr);
+            displayArray(arr);
         }
     }
-    public static void main(String args[]){
-        int arr[] = {10,22,34,57,90};
-        bubbleSort(arr);
-        displayArray(arr);
-    }
-
-}
-    
