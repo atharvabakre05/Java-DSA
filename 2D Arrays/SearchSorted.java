9f178502b257c242e2727d74d8bfa5cdbf19bcc4
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-09-18 05:06:29 +0530

    StarCase Matrix Using 1 special element

---
+++ 2D Arrays/SearchSorted.java
@@ -0,0 +1,31 @@
+public class SearchSorted{
+    public static boolean stairCaseSearch(int matrix[][], int key){
+        int row = matrix.length-1; 
+        int col = 0;
+
+        while(row>=0 && col<matrix[0].length){
+            if(matrix[row][col] == key){
+                System.out.println("Key was found at (" + row + "," + col + ")");
+                return true;
+            }
+            else if(matrix[row][col]>key){
+                row--;
+            }
+            else{
+                col++;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+}
+    
+    public static void main(String[] args){
+        int matrix[][] = {{1,2,3,4}, 
+                          {5,6,7,8}, 
+                          {9,10,11,12}, 
+                          {13,14,15,16}};
+        int key = 13;
+        
+       stairCaseSearch(matrix, key);
+    }
+}
