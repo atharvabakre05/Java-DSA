763a7a75033f5b018621cea5e9e3ff8f78df29f8
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-09-21 09:54:23 +0530

    Concatination and basics of strings

---
+++ Strings/Basics.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Basics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // String name, sentence;
+        // name = sc.next();
+        // System.out.println("Your name is: " + name);
+
+        //To find the length of the String!
+        // sentence = sc.nextLine();
+        // System.out.println(sentence.length());
+
+
+        //Concatination 
+        String firstName = ("Atharva");
+        String lastName = ("Bakre");
+
+        String fullName = firstName + " " + lastName;
+        System.out.println("My full name is: " + fullName);
+    }
+}
