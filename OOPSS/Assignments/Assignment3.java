ff13527f39dd947fedb7379c24eff2b051831ea7
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-09-21 19:11:07 +0530

    Solved assignment questions

---
+++ OOPSS/Assignments/Assignment3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Assignment3{
+
+    //Q1
+    // public static int count(int n){
+    //     int lastDigit;
+    //     int total=0;
+    //     while(n>10){
+    //         total += lastDigit;
+    //         n = n/10;
+    //     }
+    //     return total;
+    // }
+    // public static void main(String[] args) {
+    //     System.out.println("The count of all digits is: " + count(45));
+    // };
+
+
+
+    //Q2
+    public static void uniqueCount(int n){
+        int lastDigit = 0;
+        while(n>0) {
+            lastDigit = n%2;
+            if()
+        }
