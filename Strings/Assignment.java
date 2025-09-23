f0b6d96f9d933be2f580e47e2dad9ed6b217e992
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-09-23 21:22:57 +0530

    LowerVowel count strings

---
+++ Strings/Assignment.java
@@ -0,0 +1,18 @@
+public class Assignment {
+    //Q1
+    public static int lowerVowels(String str){
+        // StringBuilder sb = new StringBuilder("");
+        int count = 0;
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == 'a' ||str.charAt(i) == 'e' ||str.charAt(i) == 'i'||str.charAt(i) == 'o'||str.charAt(i) == 'u' ){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String args[]){
+        String str = "Atharva atharva";
+        System.out.println(lowerVowels(str));
+    }
+}
