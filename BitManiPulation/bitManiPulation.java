5f1fac70d113b9aa47464a32ad1bda59d72fafa8
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-10-11 09:40:28 +0530

    Revised OddEveb

--- BitManiPulation/bitManiPulation.java
+++ BitManiPulation/bitManiPulation.java
@@ -1,16 +1,17 @@
-
 import java.util.*;
-    public class bitManiPulation{
-        public static void oddOrEven(int n){
-            int bitMask = 1;
-            if((n & bitMask) == 0){
-                System.out.println("Even Number");
-            }else{
-                System.out.println("Odd Number");
-            }
-        }
-        public static void main(String args[]){
-            oddOrEven(1);
-            oddOrEven(16);
+
+public class bitManiPulation{
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if((n & bitMask) == 0) {
+            System.out.println("It's an even number");
+        }else{
+            System.out.println("It's an odd number");
         }
     }
+
+    public static void main(String[] args) {
+        oddOrEven(10011);
+        
+    }
+}
