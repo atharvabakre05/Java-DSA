b39d772a57b9a98e223ed03c4e793de385d2424f
Atharva-Bakre <atharva.bakre05@gmail.com>
2025-10-15 20:58:58 +0530

    Clear operation in BitManipulation

--- BitManiPulation/bitOperations.java
+++ BitManiPulation/bitOperations.java
@@ -14,8 +14,14 @@ public class bitOperations {
         return n|bitMask;
     }
 
+    public static int clearBit(int n, int i) {
+        int bitMask = ~(1<<i);
+        return (n&bitMask);
+    }
+
     public static void main(String[] args){
         // System.out.println(getBit(10, 2));
-        System.out.println(setBit(10, 2));
+        // System.out.println(setBit(10, 2));
+        System.out.println(clearBit(10, 1));
     }
 }
