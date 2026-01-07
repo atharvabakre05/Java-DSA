5be9a8b6ff7e1280b2d198da17ed598eafcb7152
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-07 21:56:13 +0530

    Bubble sort revision

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,23 @@
+public class bubbleSort {
+
+    public static void bubbleSorting(int arr[]){
+        // int x = arr[0];
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,44,5,22,2};
+        bubbleSorting(arr);
+
+    }
+}
