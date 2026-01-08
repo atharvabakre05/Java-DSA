4b192374d879e17711fdfd45a1449116d620870d
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-08 07:46:28 +0530

    Sortuing revision

---
+++ Sorting/reviseSort.java
@@ -0,0 +1,69 @@
+import java.util.*;
+public class reviseSort {
+
+    public static void bubbleSorting(int arr[]){
+        // int x = arr[0];
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos]>arr[j]){
+                    minPos = j;
+                }
+            }
+            //Swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    public static void insertionSort(int arr[]){
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1,2,44,5,22,2};
+        // bubbleSorting(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        Arrays.sort(arr);
+        printArray(arr);
+    }
+}
