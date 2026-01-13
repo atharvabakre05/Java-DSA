c5c6bab0dd1e5703ebd5fdaf8d14d1bef8e96e68
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-13 23:15:52 +0530

    Search in 2d Matrix

--- 2D Arrays/revision.java
+++ 2D Arrays/revision.java
@@ -1,5 +1,19 @@
 import java.util.*;
 public class revision {
+
+    public static boolean search(int arr[][], int key){
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[0].length; j++) {
+                if(arr[i][j] == key){
+                    System.out.print("found at cell (" + i +"," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found!"); 
+        return false;
+
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int arr[][] = new int [3][3];
@@ -10,6 +24,7 @@ public class revision {
             }
         }
 
+
         //output
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
@@ -17,5 +32,7 @@ public class revision {
             }
             System.out.println();
         }
+
+        search(arr, 5);
     }
 }
