d223a1a0c1f6acc1b7398ec5fb66588474697582
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-17 23:36:34 +0530

    StairCaseSearch

--- 2D Arrays/revision.java
+++ 2D Arrays/revision.java
@@ -59,17 +59,44 @@ public class revision {
 
     public static int diagonalSum(int matrix[][]){
         int sum = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i==j){
-                    sum += matrix[i][j];
-                }else if(i+j == matrix.length-1){
-                    sum += matrix[i][j];
-                }
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }else if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length;i++){
+            //pd
+            sum += matrix[i][i];
+            //sd
+            if(i!= matrix.length-1-i){
+                sum+=matrix[i][matrix.length-1-i];
             }
         }
         return sum;
     }
+
+    public static boolean stairCaseSearch(int matrix[][], int key){         //T(c) = O(m+n)
+        int row = 0, col = matrix[0].length-1;
+
+        while(row<=matrix.length-1 && col>=0){
+            if(matrix[row][col] == key){
+                System.out.println("Key found at (" + row + "," + col + ")");
+                return true;
+            }else if(matrix[row][col]<key){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -97,8 +124,11 @@ public class revision {
             {10,11,12,13}, 
             {14,15,16,17}};
         // printSpiral(matrix);
-        int res = diagonalSum(matrix);
-        System.out.println(res);
+        // int res = diagonalSum(matrix);
+        // System.out.println(res);
+        int key = 15;
+        stairCaseSearch(matrix, key);
+
 
     }
 }
