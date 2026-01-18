cf99022cb3d1ea55bb687687314905c716d71ce4
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-18 10:14:44 +0530

    Transpose of matrix

--- 2D Arrays/revQue.java
+++ 2D Arrays/revQue.java
@@ -14,7 +14,7 @@ public class revQue {
         return count;
     }
     
-
+    //Q2
     public static int sumofRow(int matrix[][]){
         int sum = 0;
         for(int i=1; i<matrix.length-1; i++){
@@ -25,9 +25,22 @@ public class revQue {
         return sum;
     }
 
+    //Q3
+    public static void transPose(int matrix[][]){
+        for(int i=0; i<matrix[0].length; i++){  //columns
+            for(int j=0; j<matrix.length; j++){ //rows
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]) {
-        int matrix[][] = {{4,7,8}, {8,8,7}, {2,7,9}};
-        System.out.print(sumofRow(matrix));
-        
+        // int matrix[][] = {{4,7,8}, {8,8,7}, {2,7,9}};
+        int arr[][] = {{1,2,3}, {4,5,6}};
+
+        // System.out.print(printNumber(matrix));
+        // System.out.print(sumofRow(matrix));
+        transPose(arr);
     }
 }
