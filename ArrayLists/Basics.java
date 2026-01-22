560ce3a717758f41c0a6b4c5a1e6619eaaa1c2b2
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-01-22 11:39:05 +0530

    max,swap of ArrayList

--- ArrayLists/Basics.java
+++ ArrayLists/Basics.java
@@ -1,20 +1,78 @@
 import java.util.*;
     public class Basics{
+
+
+        public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+            int temp = list.get(idx1);
+            list.set(idx1, list.get(idx2));
+            list.set(idx2, temp);
+        
+            for(int i=0; i<list.size(); i++){
+                System.out.print(list.get(i)+ " ");
+            }
+        }   
+
         public static void main(String[] args) {
-            ArrayList <Integer> list = new ArrayList<>();
-            ArrayList <String> list2= new ArrayList<>();
-            ArrayList <Boolean> list3 = new ArrayList<>();
+            // ArrayList <Integer> list = new ArrayList<>();
+            // ArrayList <String> list2= new ArrayList<>();
+            // ArrayList <Boolean> list3 = new ArrayList<>();
             
 
-            //Add Function O(1)
-            list.add(1);
-            list.add(2);
-            list.add(3);
-            list.add(4);
-            list.add(5);
+            // //Add Function O(1)
+            // list.add(2);
+            // list.add(4);
+            // list.add(9);
+            // list.add(6);
+            // list.add(05);
 
-            list.add(2,99); //O(n)
-            System.out.println(list);
+            // list.add(2,99); //O(n)
+            // System.out.println(list);   
+
+
+            //size function of ArrayList
+            // System.out.println(list.size());
+
+
+            //To print the elements of ArrayList
+            // for(int i=0; i<list.size(); i++){
+            //     System.out.print(list.get(i) + " ");
+            // }
+
+
+            //Print Reverse of an ArrayList
+            // int n = list.size()-1;
+            // for(int i=n; i>=0; i--){
+            //     System.out.print(list.get(i)+ " ");
+            // }
+
+            //FIND MAXIMUM IN AN ARRAYLIST
+            // int max = Integer.MIN_VALUE;
+            // for(int i=0; i<list.size(); i++){
+            //     // if(max<list.get(i)){
+            //     //     max = list.get(i);
+            //     // }
+            //     max = Math.max(max, list.get(i));
+            // }
+            // System.out.print("The maximum integer in the arrayList is: " + max);
+
+
+            //SWAPPING
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(44);
+            list.add(1, 55);
+            list.add(33);
+            list.add(26);
+
+            
+            for(int i=0; i<list.size(); i++){
+                System.out.print(list.get(i)+ " ");
+            }
+            
+            int idx1 = 2;
+            int idx2 = 0;
+            System.out.println();
+            swap(list, idx1, idx2);
+
 
             //Get Function O(1)
             // int element = list.get(0);
