d7916176a01e97f52e7ef463e8033c16ca21622f
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-02-05 22:25:33 +0530

    Each word in a line to upperCase

--- Strings/Revison.java
+++ Strings/Revison.java
@@ -10,6 +10,30 @@ public class Revison {
         return true;
     }
 
+
+    public static String chartoUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            }else{
+                    sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+
+    }
+
+
+
+
+
     public static float shortestDistance(String str){
         float x = 0, y = 0;
         for(int i=0; i<str.length(); i++){
@@ -30,6 +54,16 @@ public class Revison {
     }
 
 
+    public static boolean isPalindromes(String str){
+        int n = str.length();
+        for(int i=0; i<n/2; i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.println("Enter any string line: ");
@@ -47,16 +81,28 @@ public class Revison {
 
 
         //Compare to function
+        // String str[] = {"apple", "mango", "banana"};
+        // String largest = str[0];
+        // for(int i=1; i<str.length; i++){
+        //     if(largest.compareTo(str[i])<0){
+        //         largest = str[i];
+        //     }
+        // }
+        // System.out.println(largest);
 
-        String str[] = {"apple", "mango", "banana"};
-        String largest = str[0];
-        for(int i=1; i<str.length; i++){
-            if(largest.compareTo(str[i])<0){
-                largest = str[i];
-            }
-        }
-        System.out.println(largest);
+
+        //StringBuilder
+
+        // StringBuilder str = new StringBuilder("");
+        // for(char ch='a'; ch<='z'; ch++){
+        //     str.append(ch);
+        // }
+        // System.out.println(str);
 
 
+        // System.out.println(isPalindromes("Atharva"));
+
+        String str = new String("i am atharva bakre ");
+        System.out.println(chartoUpperCase(str));
     }
 }
