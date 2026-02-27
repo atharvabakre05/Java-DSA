cb56fe9668f80e016fa08d93603179354691ff1f
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-02-27 19:30:04 +0530

    Factorial using recursion

--- Recursion/revision.java
+++ Recursion/revision.java
@@ -1,24 +1,41 @@
+import java.util.*;
 public class revision {
-    public static void printDec(int n){
-        if(n == 1){
-            System.out.println(n+ " ");
+
+    public static void decPrint(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
             return;
         }
         System.out.print(n + " ");
-        printDec(n-1);
+        decPrint(n - 1);
     }
 
-    public static void printInc(int n){
-        if(n==1){
-            System.out.print(n +" ");
+    public static void incPrint(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
             return;
         }
-        printInc(n-1);
+        incPrint(n - 1);
         System.out.print(n + " ");
     }
-    public static void main(String[] args){
-        int n = 10;
-        // printDec(n);
-        printInc(n);
+
+    public static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        int fm1 = fact(n-1);
+        int fn = n*fm1;
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        // decPrint(n);
+        // System.out.println();
+        // incPrint(n);
+        System.out.println(fact(n));
+
+
     }
 }
