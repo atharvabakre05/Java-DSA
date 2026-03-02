d2953be297d4a2b7fe78f5ac6a3ef733d64a0fdb
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-02 18:02:46 +0530

    Calculate sum using recursion

--- Recursion/revision.java
+++ Recursion/revision.java
@@ -23,18 +23,29 @@ public class revision {
         if(n == 0){
             return 1;
         }
-        int fm1 = fact(n-1);
-        int fn = n*fm1;
+        int fn1 = fact(n-1);
+        int fn = n*fn1;
         return fn;
     }
+
+    public static int calcSum(int n){
+        if(n == 1){
+            return 1;
+        }
+        int Sm1 = calcSum(n-1);
+        int Sn = n + Sm1;
+        return Sn;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 5;
         // decPrint(n);
-        // System.out.println();
+        // System.out.println();    
         // incPrint(n);
-        System.out.println(fact(n));
+        // System.out.println(fact(n));
+        System.out.println(calcSum(n));
 
 
     }
