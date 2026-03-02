c1272ae5289432ef887b2a47997b067d43e1d106
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-02 22:28:28 +0530

    fibonnaci series recursion

--- Recursion/revision.java
+++ Recursion/revision.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class revision {
 
     public static void decPrint(int n) {
@@ -19,33 +20,46 @@ public class revision {
         System.out.print(n + " ");
     }
 
-    public static int fact(int n){
-        if(n == 0){
+    public static int fact(int n) {
+        if (n == 0) {
             return 1;
         }
-        int fn1 = fact(n-1);
-        int fn = n*fn1;
+        int fn1 = fact(n - 1);
+        int fn = n * fn1;
         return fn;
     }
 
-    public static int calcSum(int n){
-        if(n == 1){
+    public static int calcSum(int n) {
+        if (n == 1) {
             return 1;
         }
-        int Sm1 = calcSum(n-1);
+        int Sm1 = calcSum(n - 1);
         int Sn = n + Sm1;
         return Sn;
     }
 
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = fib(n - 1);
+        int fnm2 = fib(n - 2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 5;
         // decPrint(n);
-        // System.out.println();    
+        // System.out.println();
         // incPrint(n);
         // System.out.println(fact(n));
-        System.out.println(calcSum(n));
+        // System.out.println(calcSum(n));
+        System.out.println(fib(25));
+        System.out.println(fib(24));
+        System.out.println(fib(23));
+
 
 
     }
