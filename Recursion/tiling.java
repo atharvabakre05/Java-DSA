fd16fe17c621ca7b07153c0636b18318c9afdc66
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-06 23:51:10 +0530

    Revision

--- Recursion/tiling.java
+++ Recursion/tiling.java
@@ -13,6 +13,7 @@ public class tiling {
         int fn = fnm1 + fnm2;
         return fn;
     }
+    public static fibonnaci
     public static void main(String[] args){
         System.out.println(tilingProblem(3));
     }
