e799dd99b3c766842d6447602d26824b8bc329a7
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-10 22:50:54 +0530

    RemoveDuplicateString

--- Recursion/removeDuplicateString.java
+++ Recursion/removeDuplicateString.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class removeDuplicateString {
-    
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]){
+        if(idx == str.length()){
+            System.out.print(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true){
+            //Duplicate
+            removeDuplicate(str, idx+1, newStr, map);
+        }else{
+            map[currChar -'a'] = true;
+            removeDuplicate(str, idx+1, newStr.append(currChar), map);
+        }
+    }
+    public static void main(String[] args){
+        String str = "apnacollege";
+        removeDuplicate(str, 0, new StringBuilder(""), new boolean[26]);
+    }
 }
