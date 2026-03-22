eb64b3580dd96c263d26cfece1b240f2035ba83b
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-22 19:05:06 +0530

    Add in middle LL

--- LinkedList/LinkedListBasics.java
+++ LinkedList/LinkedListBasics.java
@@ -1,70 +1,83 @@
 import java.util.*;
-public class LinkedListBasics{
+public class LinkedListBasics{  
     public static class Node{
         int data;
         Node next;
+
         public Node(int data){
-        this.data  = data;
-        this.next = null;
+            this.data = data;
+            this.next = null;
         }
     }
     public static Node head;
     public static Node tail;
-    
+
+    //Methods add, remove, print, search
     public void addFirst(int data){
-        //step 1 create new node
+        //step 1 = create newNode
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
             return;
         }
-
-        //step 2 newNode next = head
+        //step 2 = set newNode next value to the previous node's head
         newNode.next = head;
-
-        //step 3 - head = newNode 
+        //step 3 = set newNode as head;
         head = newNode;
     }
 
     public void addLast(int data){
-        //step 1 Create a new Node
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
             return;
         }
-        //step 2 Link
-        tail.next = newNode;
-
-        //Change the tail 
+        tail.next = newNode; 
         tail = newNode;
     }
 
-    public void print(){    //O(n)
-        // if(head == null){
-        //     System.out.println("LL is empty");
-        // }
+    public void print(){//O(n)
         Node temp = head;
-        while(temp!=null){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+
+        }
+        while(temp != null){
             System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("null");
     }
-    
+
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
 
     public static void main(String[] args){
         LinkedListBasics ll = new LinkedListBasics();
+        ll.addFirst(2);
         ll.print();
         ll.addFirst(1);
-        ll.print();
-        ll.addFirst(2);
         ll.print();
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
         ll.print();
-
+        ll.add(2,9);
+        ll.print();
     }
-    
 }
