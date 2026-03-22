2c8ba4640b9b2ae19acefe945f2de10947a48400
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-03-22 19:48:38 +0530

    RemoveFirst and RemoveLast in LL

--- LinkedList/LinkedListBasics.java
+++ LinkedList/LinkedListBasics.java
@@ -11,11 +11,13 @@ public class LinkedListBasics{
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     //Methods add, remove, print, search
     public void addFirst(int data){
         //step 1 = create newNode
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -28,6 +30,7 @@ public class LinkedListBasics{
 
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -56,6 +59,7 @@ public class LinkedListBasics{
             return;
         }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
 
@@ -67,17 +71,61 @@ public class LinkedListBasics{
         temp.next = newNode;
     }
 
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        Node prev = head;
+        for(int i=0; i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = tail.data;       //tail data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+
     public static void main(String[] args){
         LinkedListBasics ll = new LinkedListBasics();
         ll.addFirst(2);
         ll.print();
         ll.addFirst(1);
         ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
         ll.print();
-        ll.add(2,9);
+        ll.addLast(5);
         ll.print();
+        ll.add(2,3);
+        ll.print();
+        ll.removeFirst();
+        ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(size);
+        
     }
 }
