89ff4db12ee3c3141aad7f44878597cc6591dbfb
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-04-03 23:18:22 +0530

    Changes in duplicates

--- Recursion/removeDuplicateString.java
+++ Recursion/removeDuplicateString.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class removeDuplicateString{
     public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]){
-        //Base
+        //base case
         if(idx == str.length()){
             System.out.println(newStr);
             return;
@@ -11,15 +11,13 @@ public class removeDuplicateString{
         char currChar = str.charAt(idx);
         if(map[currChar - 'a'] == true){
             removeDuplicates(str, idx+1, newStr, map);
-        }
-        else{
+        }else{
             map[currChar - 'a'] = true;
             removeDuplicates(str, idx+1, newStr.append(currChar), map);
         }
     }
-
     public static void main(String[] args){
         String str = "appnnacollege";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);    
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
