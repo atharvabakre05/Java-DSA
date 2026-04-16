623de18ff7233c3331d27ef7e9b1ab56c9a2d0f2
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-04-16 20:29:21 +0530

    Quick SOrt

--- Divide And Conquer/basics.java
+++ Divide And Conquer/basics.java
@@ -6,22 +6,12 @@ public class basics{
         }
         System.out.println();
     }
-    public static void mergeSort(int arr[], int si, int ei){
-        if(si>=ei){
-            return;
-        }
-        int mid = si + (ei-si)/2;
-        mergeSort(arr, si, mid);
-        mergeSort(arr, mid+1, ei);
-        merge(arr, si, mid, ei);
-    }
-    public static void merge(int arr[],int si, int mid, int ei){
-        int temp=
-    }
+
+    SYstem.out.println("")
+
 
     public static void main(String[] args){
-        int arr[] = {6,3,4,5,2,9};
-        mergeSort(arr, 0, arr.length-1);
+        int arr[] = {6,3,9,5,2,8};
 
     }
 
