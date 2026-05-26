193e49be5be2c19413e4fa7a97dff7eee858db59
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-05-26 12:12:22 +0530

    LinkedList revision

---
+++ LinkedList/LLRevision.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class LLRevision {
+    //How a node is created!
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+    // public static Node head;
+    // public static Node tail;
+    // public static Node size;
+
+    // public static void addFirst(int data){
+    //     Node newNode = new Node(data);
+    //     size++;
+
+    // }
+    public static void main(String[] args){
+        Node head = new Node(10);
+        Node second = new Node(20);
+
+        //Linking of nodes
+        head.next = second;
+
+        System.out.println(head.data);
+        System.out.println(second.data);
+    }
+}
--- LinkedList/LinkedListBasics.java
+++ LinkedList/LinkedListBasics.java
@@ -9,6 +9,7 @@ public class LinkedListBasics{
             this.next = null;
         }
     }
+        
     public static Node head;
     public static Node tail;
     public static int size;
