fb7579b685b017f1d446cff9f3162c07ee4f2e94
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-06-19 14:12:24 +0530

    AddLast method

--- LinkedList/DoublyLL.java
+++ LinkedList/DoublyLL.java
@@ -28,6 +28,38 @@
             head = newNode;
         }
 
+        public void addLast(int data){
+            Node newNode = new Node(data);
+            size++;
+            if(head == null){
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            newNode.next = null;
+            tail = newNode;
+        }
+
+        public int removeFirst(){
+            if(head == null){
+                System.out.println("DLL is empty");
+                return Integer.MIN_VALUE;
+            }
+
+            if(size == 1){
+                int val = head.data;
+                head = tail = null;
+                size--;
+                return val;
+            }
+            
+            int val = head.data;
+            head = head.next;
+            head.prev = null;
+            size--;
+            return val;
+        }
+
         //print
         public void print(){
             Node temp = head;
@@ -40,11 +72,19 @@
 
         public static void main(String[] args){
             DoublyLL dll = new DoublyLL();
-            dll.addFirst(3);
-            dll.addFirst(2);
-            dll.addFirst(1);
+            // dll.addFirst(3);
+            // dll.addFirst(2);
+            // dll.addFirst(1);
+            dll.addLast(1);
+            dll.addLast(2);
+            dll.addLast(3);
+            dll.addLast(4);
             dll.print();
+            //1->2->3->4->5
             System.out.println(size);
+            // dll.removeFirst();
+            // dll.print();
+            // System.out.println(size);
             
         }
     }
