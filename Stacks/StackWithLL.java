34a06a3113c8e9bee9c09c9ca36b86d1f21de219
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-07-09 13:23:43 +0530

    Stack Implementation using framork

--- Stacks/StackWithLL.java
+++ Stacks/StackWithLL.java
@@ -1,56 +1,57 @@
 import java.util.*;
 public class StackWithLL{
-    public static class Node{
-        int data;
-        Node next;
+    // public static class Node{
+    //     int data;
+    //     Node next;
 
-        public Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
+    //     public Node(int data){
+    //         this.data = data;
+    //         this.next = null;
+    //     }
+    // }
 
-    public static class Stack{
-        public static Node head = null;
+    // public static class Stack{
+    //     public static Node head = null;
 
-        public static boolean isEmpty(){
-            return head == null;
-        }
+    //     public static boolean isEmpty(){
+    //         return head == null;
+    //     }
 
-        //push
-        public static void push(int data){
-            Node newNode = new Node(data);
-            if(isEmpty()){
-                head = newNode; 
-                return;
-            }
+    //     //push
+    //     public static void push(int data){
+    //         Node newNode = new Node(data);
+    //         if(isEmpty()){
+    //             head = newNode; 
+    //             return;
+    //         }
 
-            newNode.next = head;
-            head = newNode;
-            return;
-        }
+    //         newNode.next = head;
+    //         head = newNode;
+    //         return;
+    //     }
 
-        //pop
-        public static int pop(){
-            if(isEmpty()){
-                return -1;
-            }
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
+    //     //pop
+    //     public static int pop(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top = head.data;
+    //         head = head.next;
+    //         return top;
+    //     }
 
-        //peek
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
-            }
-            int top = head.data;
-            return top;
-        }
-    }
+    //     //peek
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top = head.data;
+    //         return top;
+    //     }
+    // }
     public static void main(String[] args){
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack<>();
+        // Stack s = new Stack();
         s.push(1);
         s.push(2);
         s.push(3);
