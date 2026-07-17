935cf6384767f76a78fb33d80606373c1879d721
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-07-17 11:48:06 +0530

    Stack With ArraList

--- Stacks/StackWithArrayList.java
+++ Stacks/StackWithArrayList.java
@@ -1,45 +1,40 @@
 import java.util.*;
-    public class StackWithArrayList {
-        public static class Stack{
-                static ArrayList<Integer> list = new ArrayList<>();
-                public static boolean isEmpty(){
-                    return list.size() == 0;
-                }
+public class StackWithArrayList{
+    public static ArrayList<Integer> list = new ArrayList<>();
 
-                //push function
-                public static void push(int data){
-                    list.add(data);
-                }
+    public static void push(int data){
+        list.add(data);
+    }
 
-                //pop function
-                public static int pop(){
-                    if(isEmpty()){
-                        return -1;
-                    }
+    public static int pop(){
+        if(list.isEmpty()){
+            return -1;
+        }
+        int top = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
 
-                    int top  = list.get(list.size()-1);
-                    list.remove(list.size()-1);
-                    return top;
-                }       
+    public static int peek(){
+        if(list.isEmpty()){
+            return -1;
+        }
+        int top = list.get(list.size()-1);
+        return top;
+    }
 
-                //peek
-                public static int peek(){
-                    if(isEmpty()){
-                        return -1;
-                    }
-                    return list.get(list.size()-1);
-                }
-            }
-        public static void main(String[] args){
-            Stack s = new Stack();
-            s.push(1);
-            s.push(2);
-            s.push(3);
-            s.push(4);
-
-            while(!s.isEmpty()){
-                System.out.println(s.peek());
-                s.pop();
-            }
+    public static void printAL(){
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i)+ " ");
         }
     }
+    public static void main(String[] args){
+        // Stack<Integer> s = new Stack<>();
+        StackWithArrayList obj = new StackWithArrayList();
+        push(3);
+        push(2);
+        push(1);
+        System.out.println(pop());
+        System.out.println(peek());
+    }
+}
