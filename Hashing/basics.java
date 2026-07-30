26108af994e40402c0eab6bc2b045345fa63bc2b
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-07-30 14:06:48 +0530

    Operations on hashMap

--- Hashing/basics.java
+++ Hashing/basics.java
@@ -4,11 +4,31 @@ public class basics {
         //Initialization of HashMap
         HashMap<String, Integer> hm = new HashMap<>();
 
+
         //Insert - O(1)
         hm.put("India", 100);
         hm.put("USA", 150);
         hm.put("Australia", 200);
-
         System.out.println(hm);
+
+        //Size 
+        System.out.println("The size of HashMap is: "+ hm.size());
+
+        //Is Empty
+        hm.clear();
+        System.out.println(hm.isEmpty());
+
+        //Get - O(1)
+        System.out.println(hm.get("Indonesia"));
+
+        //ContainsKey - O(1)
+        System.out.println(hm.containsKey("India"));
+        System.out.println(hm.containsKey("Indiaa"));
+
+        //Remove O(1)
+        System.out.println(hm.remove("Australia"));
+        System.out.println(hm);
+
+        
     }
 }
