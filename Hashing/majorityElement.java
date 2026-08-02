475aeb65d9a6c2e2bc455284bd5fdfa339f9d0da
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-08-02 23:15:39 +0530

    Majority Element Shortcut

--- Hashing/majorityElement.java
+++ Hashing/majorityElement.java
@@ -1,23 +1,28 @@
 import java.util.*;
-public class majorityElement {
-    public static void main(String[] args){
-        int arr[] = {1,3,2,5,1,3,1,5,1};
-        HashMap<Integer, Integer> map = new HashMap<>();
+public class majorityElement{
 
-        for(int i=0; i<arr.length; i++){
-             if(map.containsKey(arr[i])){
-                map.put(arr[i], map.get(arr[i]) + 1);  
-             }else{
-                map.put(arr[i], 1);
-             }
+    public static void main(String[] args){
+        int arr[] = {1,3,2,5,1,3,1,5,1}; 
+        int n = arr.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        // for(int i=0; i<arr.length; i++){
+        //     int nums = arr[i];
+        //     if(hm.containsKey(nums)){
+        //         hm.put(nums, hm.get(nums) + 1);
+        //     }else{
+        //         hm.put(nums, 1);
+        //     }
+        // }
+
+        for(int i=0; i<n; i++){
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
         }
 
-        Set<Integer> keySet = map.keySet();
-        for (Integer key : keySet){
-            if(map.get(key)>arr.length/3){
+        //Iteration Loop
+        for(Integer key : hm.keySet()){
+            if(hm.get(key) > n/3){
                 System.out.println(key);
             }
         }
-
     }
 }
